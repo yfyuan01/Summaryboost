dae6d89b4bb1613801ee47811a69d60ba1430f4d
renjun0hu <renjun0hu@gmail.com>
2024-07-30 07:22:56 +0000

    Fix errors for generating rules in 'car'.

--- ours/helper/serilization.py
+++ ours/helper/serilization.py
@@ -3,42 +3,8 @@
 # Serilization methods for tabular records.
 # 
 
-# def serialize_by_tabllm(row, columns, question, answer_cond, usage):
-#     # row = [f1, ..., fk, label]
-#     note = " ".join([f"The {col} is {val}." for col, val in zip(columns[:-1], row[:-1]) if val is not None])
-#     if usage == "train":
-#         """ serilization for training examples
-#         {description of the instance}
 
-#         Does this client subscribe to a term deposit?
-#         Answer: Yes
-#         """
-#         return (
-#             f"{note}\n\n"
-#             f"{question}\n"
-#             f"Answer: {answer_cond[int(row[-1])]}"
-#         )
-#     elif usage == "test":
-#         """ serilization for testing examples
-#         {description of the instance}
-        
-#         Does this client subscribe to a term deposit? Answer the question with either Yes or No.
-#         Answer: 
-#         """
-#         return (
-#             f"{note}\n\n"
-#             f"{question} {answer_cond}\n"
-#             f"Answer: "
-#         )
-#     elif usage == "sv":
-#         return (
-#             f"[[label]]: {answer_cond[int(row[-1])]}\n"
-#             f"[[feature]]: {note}"
-#         ) 
-#     else:
-#         raise ValueError(f"encounter unknown usage={usage} in serialize_by_tabllm")
-
-def serialize_by_tabllm(row:list, columns:list, question:str, answer_cond:list, is_train:bool):
+def serialize_by_tabllm(row:list, columns:list, question:str, answer_cond:list, is_train:bool, answer_requirement:str = None):
     # row = [f1, ..., fk, label]
     note = " ".join([f"The {col} is {val}." for col, val in zip(columns[:-1], row[:-1]) if val is not None])
     if is_train is True:
@@ -60,8 +26,9 @@ def serialize_by_tabllm(row:list, columns:list, question:str, answer_cond:list, 
         Does this client subscribe to a term deposit? Answer the question with either Yes or No.
         Answer:
         """
+        answer_options = '/'.join(answer_cond)
         return (
             f"{note}\n\n"
-            f"{question} {answer_cond}\n"
-            f"Answer: <xxx, Yes/No>"
+            f"{question} {answer_requirement}\n"
+            f"Answer: <xxx, {answer_options}>"
         )
